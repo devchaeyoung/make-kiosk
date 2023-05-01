7c9c21698718d34d364ffc638ccb24b7dbbef7c2
chaeyoung <chaemaa00@gmail.com>
2023-05-01 21:57:16 +0900

    230501

--- order.js
+++ order.js
@@ -1,4 +1,4 @@
-const items = [
+const cafeItems = [
   {
     name: "아메리카노",
     img: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5fuB/image/0U_zSPit7-slkh1Cq2lIlYa0Boc.jpg",
@@ -6,7 +6,7 @@ const items = [
     count: 0,
   },
   {
-    name: "아메리카노",
+    name: "카페라떼",
     img: "https://t1.daumcdn.net/thumb/R720x0/?fname=http://t1.daumcdn.net/brunch/service/user/5fuB/image/0U_zSPit7-slkh1Cq2lIlYa0Boc.jpg",
     price: 2700,
     count: 0,
@@ -37,15 +37,25 @@ const items = [
   },
 ];
 function CafeItem() {
-  const $container = document.querySelector(".container");
+  const $menuList = document.querySelector(".menu-list");
 
-  items.forEach((item) => {
+  // for(var i=0 ;i<cafeItems.length;i++){
+  //   const item = cafeItems[i];
+  // } --> forEach문과 같음
+
+  cafeItems.forEach((item) => {
     const $div = document.createElement("div");
     $div.className = "cafe-item";
+    // $div.addEventListener("click", function () {
+    //   alert(item.name);
+    //   const $SelectMenu = document.querySelector(".select-menu-name");
+    //   $SelectMenu.innerHTML = item.name;
+    // });
+
     const $img = document.createElement("img");
     $img.src = item.img;
     $div.appendChild($img);
-    $container.appendChild($div);
+    $menuList.appendChild($div);
   });
 }
 CafeItem();
